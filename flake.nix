06cb9e227d589456885644e649bcff8dea8d3aa1
Andrew Bruce <me@andrewbruce.net>
2023-10-03 00:42:13 +0100

    Basic building

--- flake.nix
+++ flake.nix
@@ -2,11 +2,18 @@
   outputs = { self, nixpkgs }: 
     let pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in {
-    packages.x86_64-linux.default = pkgs.mkShell {
+    devShells.x86_64-linux.default = pkgs.mkShell {
       packages = [
         pkgs.cargo
         pkgs.rustc
       ];
+    };
+
+    packages.x86_64-linux.default = pkgs.rustPlatform.buildRustPackage {
+      name = "git-mob";
+      src = ./.;
+      cargoSha256 = "sha256-V6KZj6gPeUarnuy+1Wes/9H39l8vjfZUHIzGqfYV0aA=";
+
     };
   };
 }
