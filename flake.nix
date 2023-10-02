54dfd2668227de370fa5dd1f8fd78437ccedfa8a
Andrew Bruce <me@andrewbruce.net>
2023-10-03 00:44:07 +0100

    Formatter

--- flake.nix
+++ flake.nix
@@ -1,19 +1,20 @@
 {
-  outputs = { self, nixpkgs }: 
+  outputs = { self, nixpkgs }:
     let pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in {
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      packages = [
-        pkgs.cargo
-        pkgs.rustc
-      ];
-    };
+      formatter.x86_64-linux = pkgs.nixpkgs-fmt;
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = [
+          pkgs.cargo
+          pkgs.rustc
+          pkgs.nixpkgs-fmt
+        ];
+      };
 
-    packages.x86_64-linux.default = pkgs.rustPlatform.buildRustPackage {
-      name = "git-mob";
-      src = ./.;
-      cargoSha256 = "sha256-V6KZj6gPeUarnuy+1Wes/9H39l8vjfZUHIzGqfYV0aA=";
-
+      packages.x86_64-linux.default = pkgs.rustPlatform.buildRustPackage {
+        name = "git-mob";
+        src = ./.;
+        cargoSha256 = "sha256-V6KZj6gPeUarnuy+1Wes/9H39l8vjfZUHIzGqfYV0aA=";
+      };
     };
-  };
 }
