d9cd5fd4419e43771b57287baca17c6546453492
Andrew Bruce <me@andrewbruce.net>
2023-10-03 00:32:11 +0100

    cargo and rustc

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  outputs = { self, nixpkgs }: 
+    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in {
+    packages.x86_64-linux.default = pkgs.mkShell {
+      packages = [
+        pkgs.cargo
+        pkgs.rustc
+      ];
+    };
+  };
+}
