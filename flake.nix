cf9d2f447920db7e0a0a99f17b85c04fa58e0ef1
Andrew Bruce <me@andrewbruce.net>
2023-12-29 13:56:54 +0000

    forAllSystems, default app.

--- flake.nix
+++ flake.nix
@@ -1,9 +1,45 @@
 {
   outputs = { self, nixpkgs }:
-    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in {
-      formatter.x86_64-linux = pkgs.nixpkgs-fmt;
-      devShells.x86_64-linux.default = pkgs.callPackage ./shell.nix { };
-      packages.x86_64-linux.default = pkgs.callPackage ./. { };
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      forAllSystems = generate: nixpkgs.lib.genAttrs [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ]
+        (system: generate (
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+            callPackage = pkgs.lib.callPackageWith
+              (pkgs // {
+                version =
+                  if self ? rev
+                  then self.rev
+                  else "dirty";
+              });
+          in
+          {
+            inherit pkgs callPackage;
+          }
+        ));
+    in
+    {
+      formatter = forAllSystems ({ pkgs, ... }: pkgs.nixpkgs-fmt);
+
+      apps = forAllSystems ({ callPackage, ... }: {
+        default = {
+          type = "app";
+          program = callPackage ./. { };
+        };
+      });
+
+      packages = forAllSystems ({ callPackage, ... }: {
+        default = callPackage ./. { };
+      });
+
+      devShells = forAllSystems ({ callPackage, ... }: {
+        default = callPackage ./shell.nix { };
+      });
     };
 }
