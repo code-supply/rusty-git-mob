a3b29e498d1e8be750a2f6a431dc62ce9f863c32
Andrew Bruce <me@andrewbruce.net>
2024-04-01 08:43:32 +0100

    Use cargoLock.lockFile instead of cargoHash
    Avoids having to update the hash each time a cargo change is made.

--- default.nix
+++ default.nix
@@ -8,7 +8,9 @@ with pkgs;
 rustPlatform.buildRustPackage {
   name = "rusty-git-mob";
   src = ./.;
-  cargoHash = "sha256-1GyG24NWIqiSMRk8I4t0aYXfvBX3nf5U46G1kjqiyQ0=";
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+  };
 
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ ncurses openssl ] ++
